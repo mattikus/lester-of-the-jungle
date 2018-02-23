89fe80bcd6c9a63d2ed988aa8ddce3227e9e6212
Matt Kemp <matt@mattikus.com>
2018-02-23 07:29:16 +0000

    Clone doesn't like pre-existing directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,18 @@ set -e -u -o pipefail
 echo ARGS="$@"
 env
 repo_url=https://github.com/mattikus/lester-of-the-jungle
-lita_dir=/run/lester
+lita_dir=/lester
+
+mkdir -p $lita_dir
+cd $lita_dir
 
 # Grab the latest copy of lester
-git clone --depth 1 $repo_url $lita_dir
+if [[ ! -d ${lita_dir}/.git ]]; then
+  git clone --depth 1 $repo_url $lita_dir
+else
+  git fetch --depth 1 origin
+  git reset --hard origin/master
+fi
 
-cd $lita_dir
 bundle install
 exec bundle exec lita "$@"
